cff31a505b4a16f84d52650ca673ecea18502188
Carmen Castro <alu0101045603@ull.edu.es>
2021-03-16 22:52:31 +0000

    Añadiendo tecnologías

---
+++ tailwind.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // Or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blue: "#2c3258",
+        mustard: "#e4b61a",
+        yellow: "#fee970",
+        grey: "#dbd7d4",
+        black: "#9b9183",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
