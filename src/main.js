2449a4dae9311fbfb669ce55fe606d564d802b92
Juan Pablo <juan_pablo_claros@hotmail.com>
2021-03-20 17:26:43 +0000

    tailwind added to the project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import "tailwindcss/tailwind.css";
+import "./assets/css/index.css";
 
 Vue.config.productionTip = false
 
