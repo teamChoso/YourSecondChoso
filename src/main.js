fd40fef446ee3963c69c88fb459cf247a150d8ca
Juan Pablo <juan_pablo_claros@hotmail.com>
2021-03-23 15:34:48 +0000

    LeafLetMap added

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import store from "./store";
 import "tailwindcss/tailwind.css";
 import "./assets/css/index.css";
 import vuetify from "./plugins/vuetify";
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
 
 Vue.config.productionTip = false;
 
