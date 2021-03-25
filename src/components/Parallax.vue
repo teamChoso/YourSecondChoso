0d863de433517a560dc6ab2a4787b38fe4d03bb8
Carmen Castro <alu0101045603@ull.edu.es>
2021-03-25 19:24:45 +0000

    Dividiendo la vista Home en componentes y modificando el acordeón para subcategorías

---
+++ src/components/Parallax.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <v-parallax :height="600" :src="img">
+        <v-layout column align-center justify-center >
+          <h1 class="mt-20 text-3xl mb-5 ">{{cat}}</h1>
+          <div class="flex-col justify-center bg-gray-400  bg-opacity-40 w-68 p-5 rounded-lg">
+          <v-row class="w-60" justify="center">
+            <v-expansion-panels class="w-60 accordion elevation-0">
+              <v-expansion-panel class="elevation-0" style="background-color: rgba(255,255,255,0);">
+                <v-expansion-panel-header class="bg-mustard text-white rounded-full py-3 px-6" @click="pressedCatUno">{{strShow}}  subcategorías</v-expansion-panel-header>
+                <v-expansion-panel-content class="text-white py-2">
+                  <p v-for="(item,index) in subCat" :key="index">{{ item }}</p>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-row>
+          </div>
+          <!-- <v-btn rounded class="text-white texto-blanco bg-mustard-500 hover:bg-mustard-700 w-60"  @click="pressedCatUno" color="#e4b61a">{{strShow}} subcategorias</v-btn > -->
+          <div class="flex-col justify-center bg-gray-400 bg-opacity-40 w-40 p-5 rounded" v-if="mostrarCatUno" >
+            <p>Guachinches</p>
+            <p>Italianos</p>
+          </div>
+        </v-layout>
+        </v-parallax>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      strShow: "Mostrar",
+    };
+  },
+  props: {
+    img: {
+      type: String,
+      default: "",
+    },
+    cat: {
+      type: String,
+      default: "",
+    },
+    subCat: {
+      type: Array,
+      default: null,
+    },
+  },
+  methods: {
+    pressedCatUno () {
+      // (this.mostrarCatUno ? this.mostrarCatUno = false : this.mostrarCatUno = true);
+      (this.strShow === "Mostrar" ? this.strShow = "Ocultar" : this.strShow = "Mostrar");
+    },
+    pressedCatDos () {
+      // (this.mostrarCatDos ? this.mostrarCatDos = false : this.mostrarCatDos = true);
+      (this.strShow === "Mostrar" ? this.strShow = "Ocultar" : this.strShow = "Mostrar");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-expansion-panel::before {
+   box-shadow: none !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,31 @@
 <template>
   <div>
-    <template>
-      <div id="cat1"></div>
-      <v-parallax src="https://deih43ym53wif.cloudfront.net/la-geria-wine-grow-lanzarote-canary-islands-shutterstock_661595281_34f3af3ad8.jpeg">
-        <v-layout column align-center justify-center>
-          <h1 class="mt-20 text-3xl mb-5">Restaurantes</h1>
-          <v-btn rounded class="text-white texto-blanco bg-mustard-500 hover:bg-mustard-700 w-60"  @click="pressedCatUno" color="#e4b61a">{{strShow}} subcategorias</v-btn >
-          <div class="flex-col justify-center bg-gray-400 bg-opacity-40 w-40 p-5 rounded" v-if="mostrarCatUno" >
-            <p>Guachinches</p>
-            <p>Italianos</p>
-          </div>
-        </v-layout>
-      </v-parallax>
-      <div id="cat2"></div>
-      <v-parallax src="https://entremontanas.com/wp-content/uploads/tour_img-1380315-99.jpg">
-      <v-layout column align-center justify-center>
-          <h1  class="mt-20 text-3xl mb-5">Playas</h1>
-          <v-btn rounded class="text-white texto-blanco bg-mustard-500 hover:bg-mustard-700 w-60"  @click="pressedCatDos" color="#e4b61a">{{strShow}} subcategorias</v-btn >
-          <div class="flex-col justify-center bg-gray-400 bg-opacity-40 w-40 p-5 rounded" v-if="mostrarCatDos" >
-            <p>Mesa del Mar</p>
-            <p>Las Teresitas</p>
-          </div>
-        </v-layout>
-      </v-parallax>
-      <div id="cat3"></div>
-      <v-parallax src="https://s0.blt.ro/guidetocanaryislands.com/img/o/media/grancanaria/events/carnival/las-palmas-de-gran-canaria/las-palmas-de-gran-canaria-carnival-spain-1200-675.1594383467.jpg"></v-parallax>
-      <div id="cat4"></div>
-      <v-parallax src="https://f.intersailclub.com//logos/4/f/e/4fe7e0544920140110d9c9e056a037115b8ed8876b2bd-ab25e-fit-1920-1080.jpg"></v-parallax>
-      <a href="#top">Go up</a>
-      <!-- <a href="#top" v-smooth-scroll>Go up</a> -->
-</template>
+    <div id="cat1"></div>
+    <Parallax cat="Restaurantes" :subCat="subcat1" img="https://deih43ym53wif.cloudfront.net/la-geria-wine-grow-lanzarote-canary-islands-shutterstock_661595281_34f3af3ad8.jpeg"/>
+    <div id="cat2"></div>
+    <Parallax cat="Playas" :subCat="subcat2" img="https://entremontanas.com/wp-content/uploads/tour_img-1380315-99.jpg"/>
+    <div id="cat3"></div>
+    <Parallax cat="Cat3" :subCat="subcat3" img="https://s0.blt.ro/guidetocanaryislands.com/img/o/media/grancanaria/events/carnival/las-palmas-de-gran-canaria/las-palmas-de-gran-canaria-carnival-spain-1200-675.1594383467.jpg"/>
+    <div id="cat4"></div>
+    <Parallax cat="Cat4" :subCat="subcat4" img="https://f.intersailclub.com//logos/4/f/e/4fe7e0544920140110d9c9e056a037115b8ed8876b2bd-ab25e-fit-1920-1080.jpg"/>
+    <a href="#top">Go up</a>
+    <!-- <a href="#top" v-smooth-scroll>Go up</a> -->
   </div>
 </template>
 
 <script>
+import Parallax from "../components/Parallax";
 
 export default {
   data () {
     return {
-      strShow: "Mostrar",
-      mostrarCatUno: false,
-      mostrarCatDos: false,
+      subcat1: ["Guachinches", "Italianos"],
+      subcat2: ["Las Teresitas", "Mesa del Mar"],
     };
-  },
-  methods: {
-    pressedCatUno () {
-      (this.mostrarCatUno ? this.mostrarCatUno = false : this.mostrarCatUno = true);
-      (this.strShow === "Mostrar" ? this.strShow = "Ocultar" : this.strShow = "Mostrar");
-    },
-    pressedCatDos () {
-      (this.mostrarCatDos ? this.mostrarCatDos = false : this.mostrarCatDos = true);
-      (this.strShow === "Mostrar" ? this.strShow = "Ocultar" : this.strShow = "Mostrar");
-    },
   },
   name: "Home",
   components: {
+    Parallax,
   },
 };
 </script>
@@ -67,9 +38,4 @@ a{
   z-index:9999;
   border: 10px;
   }
-
-.texto-blanco {
-  color:white;
-}
-
 </style>
