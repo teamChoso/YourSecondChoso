8e7e55bba7ff4bfc655cbff2703343a4185eff43
Bruno Aggierni <bruno.aggierni@gmail.com>
2021-04-06 21:38:22 +0100

    some progress added

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,127 +1,61 @@
 <template>
-  <div class="container">
-    <v-combobox
-      class="mx-auto w-8/12"
-      v-model="chips"
-      :items="items"
-      chips
-      clearable
-      label="Seleccione la isla"
-      multiple
-      prepend-icon="mdi-filter-variant"
-      solo
-    >
-      <template v-slot:selection="{ attrs, item, select, selected }">
-        <v-chip
-          v-bind="attrs"
-          :input-value="selected"
-          close
-          @click="select"
-          @click:close="remove(item)"
-        >
-          <strong>{{ item }}</strong>
-        </v-chip>
-      </template>
-    </v-combobox>
-
-    <div class="flex justify-center flex-wrap">
-      <div v-for="(item,key) in objectDatabase" class="m-10" :key="key">
-        <v-card
-          v-if="(chips.includes(item.island)) || (chips.length === 0)"
-          width="350"
-          elevation="24"
-          outlined
-          shaped
-        >
-          <v-img
-            height="250"
-            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-          ></v-img>
-
-          <v-card-title>{{key}}</v-card-title>
-
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="2.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 text-xl">
-              {{item.municipality}}
-            </div>
-            <div><span class="font-bold">Dirección:</span> {{item.address}}</div>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn
-              color="deep-purple lighten-2"
-              text
-              @click="updateCenterMap([Number(item.latitude), Number(item.longitude)])"
-            >
-              Centrar en el mapa
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </div>
-    </div>
-    <div class="flex justify-center">
-      <LeafLetMap :chips="chips"/>
+<div class="container">
+  <div class="text-center my-20">
+    <h1 class=" title-font  mb-4 text-4xl font-extrabold leading-10 tracking-tight sm:text-5xl sm:leading-none md:text-6xl">About Us</h1>
+    <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quaerat soluta temporibus libero alias sint similique rem eos minima pariatur, sequi perspiciatis dolore tempore possimus ipsa molestias corrupti! Quis fuga repellendus pariatur earum accusantium cumque! Ad earum id iure minima temporibus deserunt, perferendis reiciendis magnam asperiores non sequi ducimus dignissimos.</p>
+    <div class="flex mt-6 justify-center">
+      <div class="w-40 h-1 rounded-full bg-indigo-500 inline-flex"></div>
     </div>
   </div>
 
+  <!-- First Profile  -->
+  <div class="grid grid-cols-4 my-5">
+    <div class="col-span-3 h-64">
+        <div class="flex justify-around items-center">
+
+          <img src="https://http.cat/405" class="h-64" alt="">
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eveniet placeat voluptatum accusamus quia et laborum,
+            saepe explicabo, quibusdam quidem facilis nemo dicta perferendis dolores quaerat expedita natus ducimus hic.</p>
+
+        </div>
+    </div>
+  </div>
+
+<!-- Second Profile -->
+  <div class="grid grid-cols-4 my-5">
+    <div class="col-start-2 col-span-3 h-64 bg-yellow">
+        <div class="flex justify-around items-center">
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eveniet placeat voluptatum accusamus quia et laborum,
+            saepe explicabo, quibusdam quidem facilis nemo dicta perferendis dolores quaerat expedita natus ducimus hic.</p>
+
+          <img src="https://http.cat/405" class="h-64" alt="">
+
+        </div>
+    </div>
+  </div>
+
+<!-- Third Profile -->
+  <div class="grid grid-cols-4 my-5">
+    <div class="col-span-3 h-64">
+        <div class="flex justify-around items-center">
+
+          <img src="https://http.cat/405" class="h-64" alt="">
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eveniet placeat voluptatum accusamus quia et laborum,
+            saepe explicabo, quibusdam quidem facilis nemo dicta perferendis dolores quaerat expedita natus ducimus hic.</p>
+
+        </div>
+    </div>
+  </div>
+
+</div>
 </template>
 
 <script>
-import LeafLetMap from "../components/LeafLetMap";
-import { mapGetters, mapActions } from "vuex";
-
 export default {
-  name: "About Us",
-  data () {
-    return {
-      chips: [],
-      items: [],
-    };
-  },
-  components: {
-    LeafLetMap,
-  },
-  computed: {
-    ...mapGetters({
-      objectDatabase: "categoryObjectDatabase",
-    }),
-  },
-  methods: {
-    remove (item) {
-      this.chips.splice(this.chips.indexOf(item), 1);
-      this.chips = [...this.chips];
-    },
-    assignCategories () {
-      Object.entries(this.objectDatabase).forEach(([key, value]) => {
-        if (!this.items.includes(value.island)) {
-          this.items.push(value.island);
-        }
-      });
-      console.log(this.items);
-    },
-    ...mapActions(["updateCenterMap"]),
-  },
-  update () {
-    this.assignCategories();
-  },
+
 };
 </script>
 
