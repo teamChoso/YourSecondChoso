26c08fc17f5ccd74b76e8d06ca9d6a1b7c25fe8e
Juan Pablo <juan_pablo_claros@hotmail.com>
2021-04-10 22:30:30 +0100

    view succes and failed added to stripe component

---
+++ src/views/Failed/Failed.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container bg-grey w-1/2 mt-6 pb-5 rounded-md">
+    <h1 class="text-4xl font-bold my-2">Compra fallida</h1>
+    <h2 class="text-2xl mb-3">Ha ocurrido un problema con su compra, lo siento</h2>
+    <PrimaryButton
+      name="Volver a la página"
+      @click.native="returnIndex"
+    />
+  </div>
+</template>
+
+<script>
+import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
+
+export default {
+  methods: {
+    returnIndex () {
+      this.$router.replace({ path: "/" });
+    },
+  },
+  components: {
+    PrimaryButton,
+  },
+};
+</script>
+
+<style>
+
+</style>
