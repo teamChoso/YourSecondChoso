5bb2bffd3460de92c4efa1b1986261a2de743661
Bruno Aggierni <bruno.aggierni@gmail.com>
2021-04-13 21:44:03 +0100

    About us modified

--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -1,56 +1,68 @@
 <template>
 <div class="container">
+  <!-- Title -->
   <div class="text-center my-20">
     <h1 class=" title-font  mb-4 text-4xl font-extrabold leading-10 tracking-tight sm:text-5xl sm:leading-none md:text-6xl">About Us</h1>
     <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quaerat soluta temporibus libero alias sint similique rem eos minima pariatur, sequi perspiciatis dolore tempore possimus ipsa molestias corrupti! Quis fuga repellendus pariatur earum accusantium cumque! Ad earum id iure minima temporibus deserunt, perferendis reiciendis magnam asperiores non sequi ducimus dignissimos.</p>
     <div class="flex mt-6 justify-center">
       <div class="w-40 h-1 rounded-full bg-indigo-500 inline-flex"></div>
-      </div>
+    </div>
   </div>
 
   <!-- First Profile  -->
   <div class="grid grid-cols-4 my-5">
-    <div class="col-span-3 h-64">
-        <div class="flex justify-around items-center">
-
-          <img src="https://http.cat/405" class="h-64" alt="">
-
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eveniet placeat voluptatum accusamus quia et laborum,
-            saepe explicabo, quibusdam quidem facilis nemo dicta perferendis dolores quaerat expedita natus ducimus hic.</p>
-
+    <div class="col-span-3 h-64 rounded">
+      <div class="grid grid-cols-3">
+        <div class="col-span-1 h-64 mr-4 rounded"><img src="https://http.cat/405" class="h-64 rounded" alt=""></div>
+        <div class="col-span-2 h-64 text-left">
+         <h1 class="text-xl  text-blue font-bold">Pedro Gonzalez</h1>
+         <h2 class="text-xl mb-4 text-mustard">Web Developer</h2>
+         <p class="mr-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, facilis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, accusantium?</p>
         </div>
+      </div>
     </div>
   </div>
+
+<!-- break line -->
+    <div class="flex mt-6 justify-center">
+      <div class="w-3/4 h-1 rounded-full bg-gray-300 inline-flex"></div>
+    </div>
 
 <!-- Second Profile -->
   <div class="grid grid-cols-4 my-5">
-    <div class="col-start-2 col-span-3 h-64 bg-yellow">
-        <div class="flex justify-around items-center">
-
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eveniet placeat voluptatum accusamus quia et laborum,
-            saepe explicabo, quibusdam quidem facilis nemo dicta perferendis dolores quaerat expedita natus ducimus hic.</p>
-
-          <img src="https://http.cat/405" class="h-64" alt="">
-
+    <div class="col-start-2 col-span-4 h-64">
+        <div class="grid grid-cols-3 rounded">
+          <div class="col-span-2 h-64 text-right">
+            <h1 class="text-xl  text-blue font-bold">Pedro Gonzalez</h1>
+            <h2 class="mb-4 text-mustard">Web Developer</h2>
+            <p class="ml-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, facilis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, accusantium?</p>
+          </div>
+          <div class="col-span-1 h-64 bg-gray-600 ml-4 rounded"><img src="https://http.cat/405" class="h-64 rounded" alt=""></div>
         </div>
     </div>
   </div>
 
+<!-- break line -->
+    <div class="flex mt-6 justify-center">
+      <div class="w-3/4 h-1 rounded-full bg-gray-300 inline-flex"></div>
+    </div>
+
 <!-- Third Profile -->
   <div class="grid grid-cols-4 my-5">
-    <div class="col-span-3 h-64">
-        <div class="flex justify-around items-center">
-
-          <img src="https://http.cat/405" class="h-64" alt="">
-
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eveniet placeat voluptatum accusamus quia et laborum,
-            saepe explicabo, quibusdam quidem facilis nemo dicta perferendis dolores quaerat expedita natus ducimus hic.</p>
-
+    <div class="col-span-3 h-64 rounded">
+      <div class="grid grid-cols-3">
+        <div class="col-span-1 h-64 mr-4 rounded"><img src="https://http.cat/405" class="h-64 rounded" alt=""></div>
+        <div class="col-span-2 h-64 text-left">
+         <h1 class="text-xl  text-blue font-bold">Pedro Gonzalez</h1>
+         <h2 class="text-xl mb-4 text-mustard">Web Developer</h2>
+         <p class="mr-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, facilis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, accusantium?</p>
         </div>
+      </div>
     </div>
   </div>
 
 </div>
+
 </template>
 
 <script>
