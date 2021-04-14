7145649020533bb5a1d1e7505540899109cf7300
Bruno Aggierni <bruno.aggierni@gmail.com>
2021-04-14 18:06:57 +0100

    Nosotros done

--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="container">
-  <!-- Title -->
+  <!-- About Us Title -->
   <div class="text-center my-20">
-    <h1 class=" title-font  mb-4 text-4xl font-extrabold leading-10 tracking-tight sm:text-5xl sm:leading-none md:text-6xl">About Us</h1>
-    <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quaerat soluta temporibus libero alias sint similique rem eos minima pariatur, sequi perspiciatis dolore tempore possimus ipsa molestias corrupti! Quis fuga repellendus pariatur earum accusantium cumque! Ad earum id iure minima temporibus deserunt, perferendis reiciendis magnam asperiores non sequi ducimus dignissimos.</p>
+    <h1 class=" title-font  mb-4 text-4xl font-extrabold leading-10 tracking-tight sm:text-5xl sm:leading-none md:text-6xl">Sobre nosotros</h1>
+    <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Donde las sociedades se juntan. Donde la diversidad es infinita. Donde la felicidad es un don innato y la tranquilidad reina. Desde estas tierras africanas, con carácter europeo y  pasión latina, te queremos mostrar lo mejor de las islas. <br>Nosotros, junto a <strong>Your Second Choso</strong>, seremos tu guía por ese camino que tanto tiene por descubrir. </p>
     <div class="flex mt-6 justify-center">
       <div class="w-40 h-1 rounded-full bg-indigo-500 inline-flex"></div>
     </div>
@@ -13,7 +13,7 @@
   <div class="grid grid-cols-4 my-5">
     <div class="col-span-3 h-64 rounded">
       <div class="grid grid-cols-3">
-        <div class="col-span-1 h-64 mr-4 rounded"><img src="https://http.cat/405" class="h-64 rounded" alt=""></div>
+        <div class="col-span-1"><img src="../../assets/About/jp_juanpa.jpg" class="h-64 w-72 rounded-2xl" alt=""></div>
         <div class="col-span-2 h-64 text-left">
          <h1 class="text-xl  text-blue font-bold">Pedro Gonzalez</h1>
          <h2 class="text-xl mb-4 text-mustard">Web Developer</h2>
@@ -34,10 +34,10 @@
         <div class="grid grid-cols-3 rounded">
           <div class="col-span-2 h-64 text-right">
             <h1 class="text-xl  text-blue font-bold">Pedro Gonzalez</h1>
-            <h2 class="mb-4 text-mustard">Web Developer</h2>
+            <h2 class="mb-4 text-xl text-mustard">Web Developer</h2>
             <p class="ml-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, facilis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, accusantium?</p>
           </div>
-          <div class="col-span-1 h-64 bg-gray-600 ml-4 rounded"><img src="https://http.cat/405" class="h-64 rounded" alt=""></div>
+          <div class="col-span-1 flex justify-end"><img src="../../assets/About/ca_carmen.jpg" class="h-64 w-72 rounded-2xl" alt=""></div>
         </div>
     </div>
   </div>
@@ -51,7 +51,7 @@
   <div class="grid grid-cols-4 my-5">
     <div class="col-span-3 h-64 rounded">
       <div class="grid grid-cols-3">
-        <div class="col-span-1 h-64 mr-4 rounded"><img src="https://http.cat/405" class="h-64 rounded" alt=""></div>
+        <div class="col-span-1 h-64"><img src="../../assets/About/br_bruno.jpg" class="h-64 w-72 rounded-2xl" alt=""></div>
         <div class="col-span-2 h-64 text-left">
          <h1 class="text-xl  text-blue font-bold">Pedro Gonzalez</h1>
          <h2 class="text-xl mb-4 text-mustard">Web Developer</h2>
